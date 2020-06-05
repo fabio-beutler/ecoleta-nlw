3d6a89cd68fd761485e78402090f0ac46ba68321
Fabio Beutler <dev.fabiobeutler@gmail.com>
2020-06-05 17:14:32 -0400

    Added DB queries and release v1.0

--- src/database/db.js
+++ src/database/db.js
@@ -4,6 +4,8 @@ const sqlite3 = require("sqlite3").verbose();
 // Iniciar o objeto de banco de dados
 const db = new sqlite3.Database("./src/database/database.db");
 
+module.exports = db;
+
 // // Utilizar o objeto de banco de dados, para nossas operações
 // db.serialize(() => {
 //   // Com comandos SQL eu vou:
--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 
+// Pegar o banco de dados
+const db = require("./database/db");
+
 // Configurar pasta pública
-server.use(express.static('public'));
+server.use(express.static("public"));
+
+// Habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }));
 
 // Utilizando o template engine
-const nunjucks = require('nunjucks');
-nunjucks.configure('src/views', {
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
   express: server,
   noCache: true
 });
@@ -15,16 +21,81 @@ nunjucks.configure('src/views', {
 // página inicial
 // REQ: Requisição
 // RES: Resposta
-server.get('/', (req, res) => {
-  return res.render('index.html');
+server.get("/", (req, res) => {
+  return res.render("index.html");
 });
 
-server.get('/create-point', (req, res) => {
-  return res.render('create-point.html');
+server.get("/create-point", (req, res) => {
+  //req.query: Query string da nossa url
+  // console.log(req.query);
+
+  return res.render("create-point.html");
 });
 
-server.get('/search', (req, res) => {
-  return res.render('search-results.html');
+server.post("/savepoint", (req, res) => {
+  // req.body: corpo do formulário
+  // Inserir dados do DB
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?,?,?,?,?,?,?);
+  `;
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ];
+
+  function afterInsertData(err) {
+    if (err) {
+      console.log(err);
+      return res.send("Erro no Cadastro!");
+    }
+
+    console.log("Cadastrado com sucesso");
+    console.log(this);
+
+    return res.render("create-point.html", { saved: true });
+  }
+
+  db.run(query, values, afterInsertData);
+});
+
+server.get("/search", (req, res) => {
+  const search = req.query.search;
+
+  if (search == "") {
+    // Pesquisa vazia
+    return res.render("search-results.html", { total: 0 });
+  }
+  // Pegar os dados do banco de dados
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (
+    err,
+    rows
+  ) {
+    if (err) {
+      console.log(err);
+    }
+
+    const total = rows.length;
+
+    // Mostrar a página html com os dados do banco de dados
+    return res.render("search-results.html", { places: rows, total });
+
+    console.log("Aqui estão seus registros");
+    console.log(rows);
+  });
 });
 
 // Ligar o servidor
